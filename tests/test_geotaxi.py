8d345f6a986166db5b83989a8d3edfaf3bcc0173
Hervé Cauwelier <herve.cauwelier@beta.gouv.fr>
2021-03-10 10:45:31 +0100

    Revert "reuse requests_mock builtin fixture"
    This reverts commit 9bf67a7649bf748771d7aaf12494d47c7e8b4fd2.

--- tests/test_geotaxi.py
+++ tests/test_geotaxi.py
@@ -1,15 +1,16 @@
+import time
 from unittest import mock
 
 import fakeredis
 import pytest
 import requests
+import requests_mock
 
 from geotaxi.worker import Worker
 
 
 class MockFluent:
     """Fake FluentSender."""
-
     def __init__(self):
         self._records = []
 
@@ -19,83 +20,86 @@ class MockFluent:
 
 class TestWorker:
 
-    def test_geotaxi_error_5xx(self, requests_mock):
-        requests_mock.get('http://api.tests/users', status_code=500)
+    def test_geotaxi_error_5xx(self):
+        with requests_mock.mock() as mock:
+            mock.get('http://api.tests/users', status_code=500)
 
-        with pytest.raises(requests.exceptions.HTTPError):
-            Worker(
+            with pytest.raises(requests.exceptions.HTTPError):
+                worker = Worker(
+                    None,
+                    auth_enabled=True,
+                    api_url='http://api.tests',
+                    api_key='f4k3'
+                )
+
+    def test_get_api_users_ok(self):
+        with requests_mock.mock() as mock:
+            mock.get('http://api.tests/users', json={
+                'data': [
+                    {'name': 'user1', 'apikey': 'key1'},
+                    {'name': 'user2', 'apikey': 'key2'},
+                ]
+            })
+
+            worker = Worker(
                 None,
                 auth_enabled=True,
                 api_url='http://api.tests',
                 api_key='f4k3'
             )
+            users = worker.get_api_users()
+            assert len(users) == 2
+            assert 'user1' in users and users['user1'] == 'key1'
+            assert 'user2' in users and users['user2'] == 'key2'
 
-    def test_get_api_users_ok(self, requests_mock):
-        requests_mock.get('http://api.tests/users', json={
-            'data': [
-                {'name': 'user1', 'apikey': 'key1'},
-                {'name': 'user2', 'apikey': 'key2'},
-            ]
-        })
+    def test_check_hash(self):
+        with requests_mock.mock() as mock:
+            mock.get('http://api.tests/users', json={
+                'data': [
+                    {'name': 'user1', 'apikey': 'key1'},
+                ]
+            })
+            redis = fakeredis.FakeStrictRedis()
+            worker = Worker(
+                redis,
+                auth_enabled=True,
+                api_url='http://api.tests',
+                api_key='f4k3'
+            )
+            is_valid = worker.check_hash({
+                'timestamp': '1',
+                'operator': 'user1',
+                'taxi': 'taxi',
+                'lat': '17',
+                'lon': '18',
+                'device': 'mobile',
+                'status': 'free',
+                'version': '1',
+                'hash': '63f3d6cf5f25e96bd085aca81d715a695c9c36e2'
+            }, ('127.0.2.3', 9999))
+            assert is_valid is True
 
-        worker = Worker(
-            None,
-            auth_enabled=True,
-            api_url='http://api.tests',
-            api_key='f4k3'
-        )
-        users = worker.get_api_users()
-        assert len(users) == 2
-        assert 'user1' in users and users['user1'] == 'key1'
-        assert 'user2' in users and users['user2'] == 'key2'
+            is_valid = worker.check_hash({
+                'timestamp': '1',
+                'operator': 'user1',
+                'taxi': 'taxi',
+                'lat': '17',
+                'lon': '18',
+                'device': 'mobile',
+                'status': 'free',
+                'version': '1',
+                'hash': 'b4dhash'
+            }, ('127.0.2.3', 9999))
+            assert is_valid is False
 
-    def test_check_hash(self, requests_mock):
-        requests_mock.get('http://api.tests/users', json={
-            'data': [
-                {'name': 'user1', 'apikey': 'key1'},
-            ]
-        })
-        redis = fakeredis.FakeStrictRedis()
-        worker = Worker(
-            redis,
-            auth_enabled=True,
-            api_url='http://api.tests',
-            api_key='f4k3'
-        )
-        is_valid = worker.check_hash({
-            'timestamp': '1',
-            'operator': 'user1',
-            'taxi': 'taxi',
-            'lat': '17',
-            'lon': '18',
-            'device': 'mobile',
-            'status': 'free',
-            'version': '1',
-            'hash': '63f3d6cf5f25e96bd085aca81d715a695c9c36e2'
-        }, ('127.0.2.3', 9999))
-        assert is_valid is True
+            assert b'badhash_operators' in redis.keys()
+            assert redis.zrange(b'badhash_operators', 0, -1, withscores=True) == [(b'user1', 1.0)]
 
-        is_valid = worker.check_hash({
-            'timestamp': '1',
-            'operator': 'user1',
-            'taxi': 'taxi',
-            'lat': '17',
-            'lon': '18',
-            'device': 'mobile',
-            'status': 'free',
-            'version': '1',
-            'hash': 'b4dhash'
-        }, ('127.0.2.3', 9999))
-        assert is_valid is False
+            assert b'badhash_taxis_ids' in redis.keys()
+            assert redis.zrange(b'badhash_taxis_ids', 0, -1, withscores=True) == [(b'taxi', 1.0)]
 
-        assert b'badhash_operators' in redis.keys()
-        assert redis.zrange(b'badhash_operators', 0, -1, withscores=True) == [(b'user1', 1.0)]
-
-        assert b'badhash_taxis_ids' in redis.keys()
-        assert redis.zrange(b'badhash_taxis_ids', 0, -1, withscores=True) == [(b'taxi', 1.0)]
-
-        assert b'badhash_ips' in redis.keys()
-        assert redis.zrange(b'badhash_ips', 0, -1, withscores=True) == [(b'127.0.2.3', 1.0)]
+            assert b'badhash_ips' in redis.keys()
+            assert redis.zrange(b'badhash_ips', 0, -1, withscores=True) == [(b'127.0.2.3', 1.0)]
 
     def test_parse_message(self):
         worker = Worker(None)
